1419c635bf0119300e816b49c26aa6025848db27
Silentamv <159916683+Silentamv@users.noreply.github.com>
2024-12-02 08:35:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -138,7 +138,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "upper-madalyn-silentfilestorebot-081b41a4.koyeb.app/")
+URL = environ.get("URL", "https://web-bkfqrla7ulgm.up-de-fra1-k8s-1.apps.run-on-seenode.com/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
